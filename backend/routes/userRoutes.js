b5d0ba0b2987c61efbe7761bf63a6328797a79af
Yatin1119 <yatinchokshi95@gmail.com>
2023-02-22 22:23:05 +0530

    added login service backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,5 +3,6 @@ var userController = require('../users/userController.js');
 const userRouter = express.Router();
 
 userRouter.route('/signup').post(userController.createUser)
+userRouter.route('/login').post(userController.loginUser)
 
 module.exports = userRouter;
